aa089a66145e97c5013f2da49efccf379eaff0ae
Lucas.SilverPC <lucas.pereira@silvergames.com.br>
2023-01-13 11:40:38 -0300

    creating api class basics

---
+++ lib/api.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:youtube_favorite_bloc/models/video.dart';
+
+const apiKey = "AIzaSyCEb3glJJgBP-x__ETPo99Ma6WCNUrsNB4";
+
+class Api {
+  search(String search) async {
+    http.Response response = await http.get(Uri.parse(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$apiKey&maxResults=10"));
+
+    decode(response);
+  }
+
+  List<Video> decode(http.Response response) {
+    if (response.statusCode == 200) {
+      var decoded = json.decode(response.body);
+      List<Video> videos = decoded["items"].map<Video>((map) {
+        return Video.fromJson(map);
+      }).toList();
+
+      return videos;
+    } else {
+      throw Exception("Falha ao buscar videos");
+    }
+  }
+}
