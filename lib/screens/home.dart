2e02466a2afe101134f692e463ffeaea9ae26b56
Lucas.SilverPC <lucas.pereira@silvergames.com.br>
2023-01-13 11:41:02 -0300

    creating home page estruture

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,6 +5,31 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: SizedBox(
+          height: 40,
+          child: Image.asset("assets/yt_logo.png"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          const Align(alignment: Alignment.center,
+              child: Text("0"),
+            ),
+          IconButton(
+            onPressed: () {
+              
+            },
+            icon: const Icon(Icons.star)),
+          IconButton(
+            onPressed: () {
+              
+            },
+            icon: const Icon(Icons.search))
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
